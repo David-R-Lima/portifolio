f0582ab1bb5c2408328a724900a74d072a439e4d
David R Lima <david@deividy.dev>
2026-01-15 17:01:38 -0300

    collors

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -26,7 +26,7 @@ export function Projects() {
         <div className="text-secondary-foreground">
           Built with React + vite, this client integrates with the previously mentioned API to provide a user-friendly interface for downloading and streaming content. It allows users to search for videos, create playlists, and manage their downloaded content. It also has a built-in music player for streaming audio.
         </div>
-        <div>
+        <div className="text-secondary-foreground">
           Techologies used: React + vite, tauri, tailwindcss, zustand
         </div>
         <div className="font-bold text-sm italic text-secondary">
@@ -35,8 +35,8 @@ export function Projects() {
         <div className="font-bold text-sm italic text-secondary">
           Note 2: It uses Tauri to create a desktop application, allowing you to run it on Windows, Linux, and macOS. It can also be served as a normal web app, and it also uses PWA for mobile clients.
         </div>
-        <div className="flex items-center space-x-2">
-          <p>Github repository: </p> <a className="underline hover:text-primary" href="https://github.com/David-R-Lima/tauri-ui">https://github.com/David-R-Lima/tauri-ui</a>
+        <div className="flex items-center space-x-2 text-secondary-foreground">
+          <p>Github repository: </p> <a className="underline hover:text-secondary" href="https://github.com/David-R-Lima/tauri-ui">https://github.com/David-R-Lima/tauri-ui</a>
         </div>
       </div>
       <div className="p-4 max-w-[90vw] space-y-2 rounded-lg bg-primary text-secondary">
@@ -50,8 +50,8 @@ export function Projects() {
         <div className="font-bold italic text-sm text-secondary">
           Databases suported: PostgreSQL, MySQL, MongoDB
         </div>
-        <div className="flex items-center space-x-2">
-          <p>Github repository: </p> <a className="underline hover:text-primary" href="https://github.com/David-R-Lima/bkp">https://github.com/David-R-Lima/bkp</a>
+        <div className="flex items-center space-x-2 text-secondary-foreground">
+          <p>Github repository: </p> <a className="underline hover:text-secondary" href="https://github.com/David-R-Lima/bkp">https://github.com/David-R-Lima/bkp</a>
         </div>
       </div>
     </div>
